f4f32c8ccb54a29478aa65e814c8dccb478d9540
OAO <10041465+aaaaagold@users.noreply.github.com>
2023-12-07 03:17:48 +0800

    Add files via upload

--- docs/tools/MoonBlueLegendRemake.js
+++ docs/tools/MoonBlueLegendRemake.js
@@ -1,13 +1,15 @@
 ﻿"use strict";
 
-(()=>{
+(()=>{ try{
+// hot fix
 
 
 
 { const me=document.currentScript;
-const p=me.parentNode;
+const p=me&&me.parentNode;
 if(p) p.removeChild(me);
 }
 
-})();
+}catch(e){
+} })();
 

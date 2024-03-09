a8ab06216abce31fd4c85b4f08ab1c241139e5f6
OAO <10041465+aaaaagold@users.noreply.github.com>
2024-03-10 03:53:03 +0800

    Add files via upload

--- docs/tools/MoonBlueLegendRemake.js
+++ docs/tools/MoonBlueLegendRemake.js
@@ -1,13 +1,61 @@
 ﻿"use strict";
 
 (()=>{ try{
+if(window._isHotfixDisabled) return;
+const _agold404_version=window._agold404_version;
+
 // hot fix
 
+// YEP卡條
+try{
+if(_agold404_version<'2024-03-10 0')(()=>{ let k,r,t;
+const p=Game_Battler.prototype;
+k='onAllActionsEnd';
+r=p[k]; (p[k]=function f(){
+	const rtv=f.ori.apply(this,arguments);
+	this._atbCharging=false;
+	this._atbCharge=0;
+	return rtv;
+}).ori=r;
+})();
+}catch(e){
+}
+// YEP卡條
 
+// 問卷
+try{
+(()=>{ let k,r,t;
+const p=Scene_Title.prototype;
+k='initialize';
+r=p[k]; (p[k]=function f(){
+	this.openResponseForm();
+	return f.ori.apply(this,arguments);
+}).ori=r;
+(p.openResponseForm=function f(){
+	const w=window,tw=getTopFrameWindow();
+	
+	if(tw._openResponseForm_opened) return;
+	if(!(Math.random()*f.tbl[0]<1)) return;
+	w.open("https://docs.google.com/forms/d/e/1FAIpQLSeNj8kwKvZweS1vNNq4pdfKpjijFgidpIqjcKWipeh8lsXA_Q/viewform",f.tbl[1],f.tbl[2]);
+	tw._openResponseForm_opened=true;
+}).tbl=[
+128,
+'_blank',
+"noopener,noreferrer",
+];
+})();
+}catch(e){
+}
+// 問卷
 
+// ---- ---- ---- ----
+
+try{
 { const me=document.currentScript;
 const p=me&&me.parentNode;
 if(p) p.removeChild(me);
+}
+}catch(e){
 }
 
 }catch(e){
